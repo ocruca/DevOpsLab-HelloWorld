a92ad1523b9be35800f9901489971d2e26062a38
glicerio.corvizzi <glicerio.corvizzi@yahoo.com.br>
2020-05-29 12:06:18 -0400

    Update README with Travis link

--- test.py
+++ test.py
@@ -19,7 +19,8 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        # self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "ZZTecnologia - DevOpsLab Essentials")
 
 
 if __name__ == "__main__":
